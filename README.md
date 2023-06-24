d86aee56745f27d139f4ef00c4a33939f4931ac8
Steven Lalewicz <steven.lalewicz@gmail.com>
2023-06-24 17:46:02 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Java class file reader written in Rust.
+
+Aim provide some kind of 'reflection' API over the class file.
+
+Could also turn into a version of Javap.
---
+++ tests/files/test1.java
@@ -0,0 +1,10 @@
+package com.strl.test;
+
+class test1 {
+   String field1;
+   int field2;
+
+   public static void main(String args[]) {
+       System.out.println("Hello Rust");
+   }
+}
